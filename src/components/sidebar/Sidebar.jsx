7fd467d2f8b126e9f61027290ce1584503f2a230
harishmahadhev <harishmahadhev@gmail.com>
2021-07-16 17:48:28 +0530

    added create user

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,31 +1,45 @@
-import './sidebar.css';
-import React from 'react'
-import { AccountBalance, Assessment, Home, Person, ShoppingCart } from '@material-ui/icons';
+import "./sidebar.css";
+import React from "react";
+import {
+  AccountBalance,
+  Assessment,
+  Home,
+  Person,
+  ShoppingCart,
+} from "@material-ui/icons";
+import { Link } from "react-router-dom";
 export default function Sidebar() {
-    return (
-        <div className="sidebar">
-           <div className="sidebarWrapper">
-               <div className="sidebarMenu">
-                   <h3 className="sidebarTitle">DASHBOARD</h3>
-                   <ul className="sidebarList">
-                       <li className="sidebarListItem active">
-                           <Home className="sidebarIcon" /> Home
-                       </li>
-                       <li className="sidebarListItem">
-                           <Person className="sidebarIcon" /> User
-                       </li>
-                       <li className="sidebarListItem">
-                           <ShoppingCart className="sidebarIcon" />  Product
-                       </li>
-                       <li className="sidebarListItem">
-                           <Assessment className="sidebarIcon" /> Analytics
-                       </li>
-                       <li className="sidebarListItem">
-                           <AccountBalance className="sidebarIcon" /> Accounts
-                       </li>
-                   </ul>
-               </div>
-           </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">DASHBOARD</h3>
+          <ul className="sidebarList">
+            <Link style={{ textDecoration: "none", color: "inherit" }} to="/">
+              <li className="sidebarListItem active">
+                <Home className="sidebarIcon" /> Home
+              </li>
+            </Link>
+            <Link
+              style={{ textDecoration: "none", color: "inherit" }}
+              to="/users"
+            >
+              <li className="sidebarListItem">
+                <Person className="sidebarIcon" /> User
+              </li>
+            </Link>
+            <li className="sidebarListItem">
+              <ShoppingCart className="sidebarIcon" /> Product
+            </li>
+            <li className="sidebarListItem">
+              <Assessment className="sidebarIcon" /> Analytics
+            </li>
+            <li className="sidebarListItem">
+              <AccountBalance className="sidebarIcon" /> Accounts
+            </li>
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/newuser/NewUser.jsx
+++ src/pages/newuser/NewUser.jsx
@@ -1,7 +1,42 @@
 import "./newuser.css";
-import React from "react";
-import { TextField, withStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Select,
+  Radio,
+  FormControlLabel,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  TextField,
+  withStyles,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Button,
+} from "@material-ui/core";
 import { userDetails } from "../../userContent";
+import { genders } from "./../../userContent";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  radio: {
+    "&$checked": {
+      color: "black",
+    },
+  },
+  checked: {},
+  select: {
+    "& .MuiSelect-select": {
+      color: "black",
+      backgroundColor: "transparent",
+      borderBottomColor: "black",
+    },
+  },
+}));
+
 const CssTextField = withStyles({
   root: {
     "& label.Mui-focused": {
@@ -18,6 +53,7 @@ const CssTextField = withStyles({
         borderColor: "grey",
       },
       "&.Mui-focused fieldset": {
+        outline: "none",
         borderColor: "black",
       },
     },
@@ -25,21 +61,77 @@ const CssTextField = withStyles({
 })(TextField);
 
 export default function NewUser() {
+  const classes = useStyles();
+  const [value, setValue] = useState("male");
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
     <div className="newUser">
       <h1 className="newUserTitle">New User</h1>
       <form className="newUserForm">
-        {userDetails.input.map((input, key) => {
-          return (
-            <div key={key} className="newUserItem">
-              <label>{input.label}</label>
-              <CssTextField
-                type={input.type}
-                placeholder={input.value}
-              ></CssTextField>
-            </div>
-          );
-        })}
+        <div>
+          {userDetails.input.map((input, key) => {
+            return (
+              <div key={key} className="newUserItem">
+                <label>{input.label}</label>
+                <CssTextField
+                  className="newUserInput"
+                  type={input.type}
+                ></CssTextField>
+              </div>
+            );
+          })}
+        </div>
+        <div className="newUserRight">
+          <div className="newUserRadio">
+            <FormControl component="fieldset">
+              <FormLabel
+                component="legend"
+                style={{
+                  marginBottom: "10px",
+                  fontSize: "14px",
+                  fontWeight: 400,
+                  color: "black",
+                }}
+              >
+                Gender
+              </FormLabel>
+              <RadioGroup
+                aria-label="Gender"
+                name="gender1"
+                onChange={handleChange}
+                value={value}
+              >
+                {genders.input.map((input, key) => {
+                  return (
+                    <FormControlLabel
+                      key={key}
+                      value={input.value}
+                      control={
+                        <Radio
+                          classes={{
+                            root: classes.radio,
+                            checked: classes.checked,
+                          }}
+                        />
+                      }
+                      label={input.label}
+                    />
+                  );
+                })}
+              </RadioGroup>
+            </FormControl>
+            <FormControl className={classes.select}>
+              <InputLabel>Active</InputLabel>
+              <Select>
+                <MenuItem value="yes">Yes</MenuItem>
+                <MenuItem value="no">No</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <Button className="newUserCreate">Create</Button>
+        </div>
       </form>
     </div>
   );
