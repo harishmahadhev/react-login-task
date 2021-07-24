0ba40b155b659936fb94e80ca5a3152434706610
harishmahadhev <harishmahadhev@gmail.com>
2021-07-25 00:02:50 +0530

    ..

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,27 +1,24 @@
 import { Badge, Avatar } from "@material-ui/core";
 import { NotificationsNone, Settings } from "@material-ui/icons";
 import React, { useEffect } from "react";
-import { useContext } from "react";
 import { useState } from "react";
-import { useHistory, useLocation } from "react-router-dom";
-import { storeCtx } from "../../pages/login/reducer";
+import { useLocation } from "react-router-dom";
+
 import "./topbar.css";
 
 export default function Topbar() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
-  const { dispatch } = useContext(storeCtx);
-  const history = useHistory();
+  // const { dispatch } = useContext(storeCtx);
+  // const history = useHistory();
   const location = useLocation();
 
-  const logout = () => {
-    dispatch({ type: "LOGOUT" });
-    history.push("/");
-    setUser(null);
-  };
+  // const logout = () => {
+  //   dispatch({ type: "LOGOUT" });
+  //   history.push("/");
+  //   setUser(null);
+  // };
 
   useEffect(() => {
-    const token = user?.token;
-
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
 
