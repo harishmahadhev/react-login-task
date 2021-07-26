36afe1a1333dc9ef15da9f7dd61d6ca8a91cb646
harishmahadhev <harishmahadhev@gmail.com>
2021-07-26 23:01:05 +0530

    .,

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://dashboard-harish.herokuapp.com",
 });
 export const signUp = (formdata) => API.post("/login/signup", formdata);
 export const signIn = (formdata) => API.post("/login/signin", formdata);
